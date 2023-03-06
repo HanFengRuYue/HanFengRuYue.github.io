30268666e3433165ab9acdad5683b21ca2215319
HanFengRuYue <huangkanggg@gmail.com>
2023-03-06 18:00:09 +0800

    更新slma2.0

--- FNaFGame/SLMA/src/Runtime.js
+++ FNaFGame/SLMA/src/Runtime.js
@@ -46292,7 +46292,7 @@ window['Runtime'] = (function Runtime(__can, __path){
 
 	    createList: function (file) {
 
-	        var extMaxHandle = 1;
+	        var extMaxHandle = 2;
 	        if (extMaxHandle) {
 	            this.extensions = new Array(extMaxHandle);
 	            this.numOfConditions = new Array(extMaxHandle);
